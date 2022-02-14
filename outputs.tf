f4723e31fa63245ca57471b82d437076fddf2f30
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2022-02-14 08:46:24 -0500

    Correct outputs if enabled is false
    Signed-off-by: Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "hcp_hvn_target_link" {
   description = "A unique URL identifying the HVN route."
-  value       = hcp_aws_transit_gateway_attachment.this[0].self_link
+  value       = join("", hcp_aws_transit_gateway_attachment.this.*.self_link)
 }
